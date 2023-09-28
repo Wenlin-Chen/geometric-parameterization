fbbe992a7e185926f7101a1f5fb533379ac6091c
Wenlin Chen <chen.wenlin@outlook.com>
2023-09-28 19:43:15 +0100

    clean code

--- imagenet2012/preprocess_dataset.py
+++ imagenet2012/preprocess_dataset.py
@@ -1,8 +1,8 @@
 import tensorflow_datasets as tfds
 
-builder = tfds.builder("imagenet2012", data_dir="/rds/user/wc337/hpc-work/data")
+builder = tfds.builder("imagenet2012", data_dir="data")
 dl_config = tfds.download.DownloadConfig(
-    extract_dir="/rds/user/wc337/hpc-work/data/downloads/extracted", 
-    manual_dir="/rds/user/wc337/hpc-work/data/downloads/manual",
+    extract_dir="data/downloads/extracted", 
+    manual_dir="data/downloads/manual",
 )
 builder.download_and_prepare(download_config=dl_config)
